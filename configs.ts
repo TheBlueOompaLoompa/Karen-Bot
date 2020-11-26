285c337c0886ed70d7657d514e11736c5a1cbc7d
Benjamin Nack <ben@nackfamily.com>
2020-11-26 00:13:24 -0600

    Change test action and fix config

--- configs.ts
+++ configs.ts
@@ -1,6 +1,14 @@
+function getToken() {
+    const token = Deno.env.get("KAREN_BOT_TOKEN")?.toString();
+    if(token === undefined) {
+        return "";
+    }
+    return token;
+}
+
 export default {
     // Your bot token goes here
-    token: Deno.env.get("KAREN_BOT_TOKEN"),
+    token: getToken(),
     // The default prefix for your bot. Don't worry guilds can change this later.
     prefix: "!",
 };
